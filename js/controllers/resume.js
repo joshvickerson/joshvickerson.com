09f1633af5dc86e20480964c24c7944228d72fd1
Josh Vickerson <josh@joshvickerson.com>
2015-03-16 17:40:42 -0400

    updated resume data

--- js/controllers/resume.js
+++ js/controllers/resume.js
@@ -1,3 +1,11 @@
-app.controller('ResumeController', function($scope) {
-  console.log("hi");
-});
+app.controller('ResumeController',['$scope', '$http', function($scope, $http) {
+
+  $http.get('data/resume.json')
+    .success(function(data) {
+        $scope.resume=data;
+        console.log($scope.resume);
+    })
+    .error(function(data,status,error,config){
+        $scope.resume = {"error":"failed to fetch resume data"};
+    });
+}]);
