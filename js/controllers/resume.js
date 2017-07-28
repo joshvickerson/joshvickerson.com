a2a33213b373b6557c7fc1f5d7a23ce625f87428
Josh Vickerson <jvickerson@clickspark.com>
2017-07-28 15:40:22 -0400

    update angular

--- js/controllers/resume.js
+++ js/controllers/resume.js
@@ -1,10 +1,10 @@
 app.controller('ResumeController',['$scope', '$http', function($scope, $http) {
 
   $http.get('data/resume.min.json')
-    .success(function(data) {
+    .then(function(data) {
         $scope.resume = data;
     })
-    .error(function(data,status,error,config){
+    .catch(function(data,status,error,config){
         $scope.resume = {"error":"failed to fetch resume data"};
     });
 }]);
